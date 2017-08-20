8f62b790bd1947ccfcf7bbbf40278b254564c9e5
esraa <esraa.alaarag@gmail.com>
2017-08-19 21:24:06 -0400

    Question2

---
+++ src/com/cognizant/shapes/AreaCalculator.java
@@ -0,0 +1,11 @@
+package com.cognizant.shapes;
+
+public class AreaCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle shape = new Rectangle();
+		shape.calculateArea();
+	}
+
+}
